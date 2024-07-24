d5ae73f91867c05961a27a06a2af4aa114bb5e23
nastyamara <110552988+nastyamara@users.noreply.github.com>
2024-07-24 16:21:22 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -58,7 +58,7 @@ export const updateContactController = async (req, res, next) => {
     res.json({
         status: 200,
         message: "Successfully patched a contact!",
-        data: result
+        data: result.value
     })
 }
 
